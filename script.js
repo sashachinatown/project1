ed270b393ac6d672eb7a45fd120354ecf584ba06
Sasha <kulibaba99@gmail.com>
2021-12-24 18:59:48 +0200

    first comit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log(4 + "4");
+
+let incr = 10,
+    decr = 10;
+    
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+    isClosed = false;
+
+console.log(isChecked || !isClosed);
